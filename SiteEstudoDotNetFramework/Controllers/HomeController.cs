725fe4bfef9df66e98811153fd0de43c7e1471fe
padmin <padmin@STFWOC025439-W>
2019-12-03 13:21:18 -0200

    Commit inicial

--- SiteEstudoDotNetFramework/Controllers/HomeController.cs
+++ SiteEstudoDotNetFramework/Controllers/HomeController.cs
@@ -1,8 +1,15 @@
-﻿using System;
+﻿using LocalEdwpBot.Classes;
+using LocalEdwpBot.Models;
+using Microsoft.Bot.Connector;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace SiteEstudoDotNetFramework.Controllers
 {
